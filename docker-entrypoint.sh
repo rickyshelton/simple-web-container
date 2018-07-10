549cc68e3958efaccaffd365c02041d75757788b
Ricky Shelton <rshelton@uk.ibm.com>
2018-07-10 18:04:47 +0100

    Updated entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # dynamically get the container id into index.html
-echo "Hello World " > /usr/share/nginx/html/index.html
-echo "This container was created on : $(date)" >> /usr/share/nginx/html/index.html
-echo "This request was served from container id : $(hostname)" >> /usr/share/nginx/html/index.html
+echo "<p>Hello World</p>" > /usr/share/nginx/html/index.html
+echo "<p>This container was created on : $(date)</p>" >> /usr/share/nginx/html/index.html
+echo "<p>This request was served from container id : $(hostname)</p>" >> /usr/share/nginx/html/index.html
 
 # start nginx
 nginx -g "daemon off;"

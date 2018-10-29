23aaa4b9068038bd79bc6601d8b37fb206a3cb57
Ricky Shelton <ric@rickys-mbp.sbank.uk.ibm.com>
2018-10-29 13:54:01 +0000

    updated hello world

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # dynamically get the container id into index.html
-echo "<p>Hello World</p>" > /usr/share/nginx/html/index.html
+echo "<p>Hello World from Ric</p>" > /usr/share/nginx/html/index.html
 echo "<p>This container was created on : $(date)</p>" >> /usr/share/nginx/html/index.html
 echo "<p>This request was served from container id : $(hostname)</p>" >> /usr/share/nginx/html/index.html
 

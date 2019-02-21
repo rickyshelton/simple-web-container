4f565a916d93e37e59304427c9e6854658c9defa
Ricky Shelton <ricky.shelton@gmail.com>
2019-02-21 20:25:34 +0000

    Added comment to include image build statement

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,7 @@
 # dynamically get the container id into index.html
 echo "<p>Hello World from Ric</p>" > /usr/share/nginx/html/index.html
 echo "<p>This container was created on : $(date)</p>" >> /usr/share/nginx/html/index.html
+echo "<p>This container image was built using the automated Docker Hub build pipeline" >> /usr/share/nginx/html/index.html
 echo "<p>This request was served from container id : $(hostname)</p>" >> /usr/share/nginx/html/index.html
 
 # start nginx
